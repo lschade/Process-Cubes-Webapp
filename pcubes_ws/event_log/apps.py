a05b1de2097b5778b9f521f3aab36743b9f46638
Lukas Schade <lschade@hubspot.com>
2020-06-28 19:55:58 +0200

    delete python backend

--- pcubes_ws/event_log/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventLogConfig(AppConfig):
-    name = 'event_log'
--- pcubes_ws/process_cube_view/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProcessCubeViewConfig(AppConfig):
-    name = 'process_cube_view'

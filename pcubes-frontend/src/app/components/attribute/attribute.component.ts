60f299a4f3f1162d42f321b2a3fe31881c709a06
Lukas Schade <luuuke@mailbox.org>
2024-01-06 16:06:55 +0100

    fix

--- pcubes-frontend/src/app/components/attribute/attribute.component.ts
+++ pcubes-frontend/src/app/components/attribute/attribute.component.ts
@@ -1,33 +1,36 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {DimensionAttribute} from '../../models/dimension-attribute';
-import {Dimension} from '../../models/dimension';
-import {PcsService} from '../../pcs.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { DimensionAttribute } from "../../models/dimension-attribute";
+import { Dimension } from "../../models/dimension";
+import { PcsService } from "../../pcs.service";
 
 @Component({
-  selector: 'app-attribute',
-  templateUrl: './attribute.component.html',
-  styleUrls: ['./attribute.component.scss']
+  selector: "app-attribute",
+  templateUrl: "./attribute.component.html",
+  styleUrls: ["./attribute.component.scss"],
 })
 export class AttributeComponent implements OnInit {
-
   @Input()
   dimension: Dimension;
 
   @Input()
   attribute: DimensionAttribute;
 
-  constructor(private pcsService: PcsService) {
-  }
+  deleted = false;
 
-  ngOnInit() {
-  }
+  constructor(private pcsService: PcsService) {}
+
+  ngOnInit() {}
 
   delete() {
-    this.pcsService.deleteAttribute(this.attribute).subscribe(value => {
-      const index = this.dimension.attributes.indexOf(this.attribute, 0);
-      if (index > -1) {
-        this.dimension.attributes.splice(index, 1);
-      }
-    }, error => console.log(error));
+    this.pcsService.deleteAttribute(this.attribute).subscribe(
+      () => {
+        const index = this.dimension.attributes.indexOf(this.attribute, 0);
+        if (index > -1) {
+          this.dimension.attributes.splice(index, 1);
+        }
+        this.deleted = true;
+      },
+      (error) => console.log(error)
+    );
   }
 }
